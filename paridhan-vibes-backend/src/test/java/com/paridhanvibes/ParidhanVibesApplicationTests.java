6cdcef3b9112077627845eca40065b957d50a461
Maheshparihar231 <maheshparihar20019.com>
2024-01-26 20:30:12 +0530

    backend-created

---
+++ paridhan-vibes-backend/src/test/java/com/paridhanvibes/ParidhanVibesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paridhanvibes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParidhanVibesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
